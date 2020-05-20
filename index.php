aad1dcfc30fe23112ca5f0c19fc08f87a62cdb5c
Pedro De La Villa <infoman.rafael@gmail.com>
2020-05-19 22:22:50 -0400

    nuevo cambio

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Este es un código HTML</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum exercitationem harum eligendi ut impedit, iure nesciunt magni nostrum nemo fugiat? Laudantium velit sint odio illo asperiores rerum eum illum.</p>
+    <h2>Nuevo cambio hecho por el administrador</h2>
+
+    <div><a href="#">Link que no lleva a ningún lado</a></div>
+
+    <p>Estos cambios no los tendra el Administrador solo yo jaja</p>
+    <span>Seguimos probando Git</span>
+    <?php echo("Este es un código PHP <br>")?>
+    <?php
+        $a = 5; 
+        $b = 10; 
+        $c = $a+$b; 
+        echo "La suma de $a y $b es $c";
+
+        echo "PHP tambien puede acceder a las bases de datos"
+    ?>
+
+    <ul>
+        <li>Primer cambio realizado</li>
+        <li>Desde una</li>
+        <li>Maquina virtual</li>
+    </ul>  
+
+</body>
+</html>
