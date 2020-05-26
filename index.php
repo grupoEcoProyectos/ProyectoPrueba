6c0c755db4f43f3493ec8159c74cfa8f83eccdb9
Rafael Ortiz <equipoproyectoseco@gmail.com>
2020-05-26 10:00:00 -0400

    ultima actualización

--- index.php
+++ index.php
@@ -7,15 +7,13 @@
 </head>
 <body>
     <h1>Este es un código HTML</h1>
-    <h2>Título h2 insertado por Rafael Ortiz</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorum exercitationem harum eligendi ut impedit, iure nesciunt magni nostrum nemo fugiat? Laudantium velit sint odio illo asperiores rerum eum illum.</p>
     <h2>Nuevo cambio hecho por el administrador</h2>
-
+    
     <div><a href="#">Link que no lleva a ningún lado</a></div>
 
     <p>Estos cambios no los tendra el Administrador solo yo jaja</p>
     <span>Seguimos probando Git</span>
-    <span>Nuevos cambios en el merge</span>
     <?php echo("Este es un código PHP <br>")?>
     <?php
         $a = 5; 
@@ -23,15 +21,8 @@
         $c = $a+$b; 
         echo "La suma de $a y $b es $c";
 
-        echo "PHP tambien puede acceder a las bases de datos"
-    ?>
-
-    <ul>
-        <li>Primer cambio realizado</li>
-        <li>Desde una</li>
-        <li>Maquina virtual</li>
-    </ul>  
-    <span>Todos mis cambios como desarrollador dependiente van aca abajo</span>
-    <div>Esta es una actualizacion de Rafael Ortiz</div>
+        echo "PHP tambien puede acceder a las bases de datosgi"
+     ?>
+     <span>ültima modificacion por Rafael Ortiz</span>
 </body>
 </html>
